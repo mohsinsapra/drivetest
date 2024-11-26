6901bc0a612d520bacbad2ce5385f8181a634388
Muhammad Mohsin <mohsin.sapra@gmail.com>
2024-11-26 16:10:13 +0100

    main: results screen added.

---
+++ lib/features/tests/result_screen.dart
@@ -0,0 +1,103 @@
+// result_screen.dart
+import 'package:flutter/material.dart';
+import 'package:taxi_exam_app/core/models/question.dart';
+import 'package:taxi_exam_app/features/tests/test_screen.dart';
+
+class ResultScreen extends StatelessWidget {
+  final List<Question> questions;
+  final Map<int, String> userSelections;
+  final String licenceId;
+  final String categoryId;
+  final bool hasPassed; // New parameter
+
+  const ResultScreen({
+    super.key,
+    required this.questions,
+    required this.userSelections,
+    required this.licenceId,
+    required this.categoryId,
+    required this.hasPassed, // Initialize the new parameter
+  });
+  @override
+  Widget build(BuildContext context) {
+    int correctAnswers = 0;
+
+    for (int i = 0; i < questions.length; i++) {
+      final question = questions[i];
+      final selectedOptionLabel = userSelections[i];
+
+      if (selectedOptionLabel != null &&
+          selectedOptionLabel == question.correctAnswer) {
+        correctAnswers++;
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Results'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              hasPassed ? 'You Passed!' : 'You Did Not Pass',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'You got $correctAnswers out of ${questions.length} correct!',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+              child: ListView.builder(
+                itemCount: questions.length,
+                itemBuilder: (context, index) {
+                  final question = questions[index];
+                  final selectedOptionLabel = userSelections[index];
+                  final isCorrect =
+                      selectedOptionLabel == question.correctAnswer;
+
+                  return Card(
+                    child: ListTile(
+                      title: Text('Question ${index + 1}: ${question.text}'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Your Answer: ${selectedOptionLabel ?? 'No Answer'}'),
+                          Text('Correct Answer: ${question.correctAnswer}'),
+                        ],
+                      ),
+                      trailing: Icon(
+                        isCorrect ? Icons.check_circle : Icons.cancel,
+                        color: isCorrect ? Colors.green : Colors.red,
+                      ),
+                      onTap: () {
+                        // Navigate back to the question for review
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Testscreen(
+                              questions: questions,
+                              instantMarking: true,
+                              licenceId: licenceId,
+                              categoryId: categoryId,
+                              initialQuestionIndex: index,
+                              userSelections: userSelections,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/tests/test_screen.dart
+++ lib/features/tests/test_screen.dart
@@ -6,12 +6,15 @@ import 'package:taxi_exam_app/core/widgets/explanation_widget.dart';
 import 'package:taxi_exam_app/core/widgets/option_widget.dart';
 import 'package:taxi_exam_app/core/widgets/question_widget.dart';
 import 'package:no_screenshot/no_screenshot.dart';
+import 'package:taxi_exam_app/features/tests/result_screen.dart';
 
 class Testscreen extends StatefulWidget {
   final List<Question> questions;
   final bool instantMarking;
   final String licenceId;
   final String categoryId;
+  final int initialQuestionIndex; // New parameter
+  final Map<int, String>? userSelections; // New parameter
 
   const Testscreen({
     super.key,
@@ -19,6 +22,8 @@ class Testscreen extends StatefulWidget {
     required this.instantMarking,
     required this.licenceId,
     required this.categoryId,
+    this.initialQuestionIndex = 0,
+    this.userSelections,
   });
 
   @override
@@ -30,29 +35,46 @@ class _TestscreenState extends State<Testscreen> {
   String selectedOptionId = '';
   bool showExplanation = false;
   final _noScreenshot = NoScreenshot.instance;
-
   final ApiService _apiService = ApiService();
+  Map<int, String> userSelections = {};
+
+  // Add ScrollController
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestionIndex = widget.initialQuestionIndex;
+    userSelections = widget.userSelections ?? {};
+    selectedOptionId = userSelections[currentQuestionIndex] ?? '';
+  }
 
   void _selectOption(String optionId) {
-    if (widget.instantMarking) {
-      setState(() {
-        selectedOptionId = optionId;
-        showExplanation = true;
-      });
-    }
+    // Capture the current scroll position
+    final scrollPosition = _scrollController.position.pixels;
+
+    setState(() {
+      selectedOptionId = optionId;
+      showExplanation = true;
+      userSelections[currentQuestionIndex] = optionId; // Record selection
+    });
+
+    // Restore the scroll position after the frame is rendered
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo(scrollPosition);
+    });
   }
 
   void _nextQuestion() {
     if (currentQuestionIndex < widget.questions.length - 1) {
       setState(() {
         currentQuestionIndex++;
-        selectedOptionId = '';
+        selectedOptionId = userSelections[currentQuestionIndex] ?? '';
         showExplanation = false;
       });
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('You have reached the end of the quiz!')),
-      );
+      // Show confirmation dialog
+      _showFinishConfirmationDialog();
     }
   }
 
@@ -60,7 +82,7 @@ class _TestscreenState extends State<Testscreen> {
     if (currentQuestionIndex > 0) {
       setState(() {
         currentQuestionIndex--;
-        selectedOptionId = '';
+        selectedOptionId = userSelections[currentQuestionIndex] ?? '';
         showExplanation = false;
       });
     } else {
@@ -68,6 +90,128 @@ class _TestscreenState extends State<Testscreen> {
         const SnackBar(content: Text('This is the first question!')),
       );
     }
+  }
+
+  void _showFinishConfirmationDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // User must tap a button to dismiss
+      builder: (BuildContext context) {
+        int unansweredQuestions =
+            widget.questions.length - userSelections.length;
+
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Finish Test'),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Dismiss the dialog
+                },
+              ),
+            ],
+          ),
+          content: Text(
+            unansweredQuestions > 0
+                ? 'You have $unansweredQuestions unanswered question(s). Do you still want to finish the test?'
+                : 'Do you want to finish the test?',
+          ),
+          actions: [
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+                _showResultDialog(); // Show pass/fail result
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  bool _calculateResult() {
+    int correctAnswers = 0;
+
+    for (int i = 0; i < widget.questions.length; i++) {
+      final question = widget.questions[i];
+      final selectedOptionLabel = userSelections[i];
+
+      if (selectedOptionLabel != null &&
+          selectedOptionLabel == question.correctAnswer) {
+        correctAnswers++;
+      }
+    }
+
+    double scorePercentage = (correctAnswers / widget.questions.length) * 100;
+    return scorePercentage >= 70; // Assume passing score is 70%
+  }
+
+  void _showResultDialog() {
+    bool hasPassed = _calculateResult();
+
+    showDialog(
+      context: context,
+      barrierDismissible: false, // User must tap a button to dismiss
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(hasPassed ? 'Congratulations!' : 'Test Completed'),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Dismiss the dialog
+                },
+              ),
+            ],
+          ),
+          content: Text(
+            hasPassed
+                ? 'You have passed the test.'
+                : 'You did not pass the test.',
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Go Back to Tests'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+                Navigator.of(context).pop(); // Go back to tests screen
+              },
+            ),
+            ElevatedButton(
+              child: const Text('See Results'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+                // Navigate to ResultScreen and remove previous routes
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultScreen(
+                      questions: widget.questions,
+                      userSelections: userSelections,
+                      licenceId: widget.licenceId,
+                      categoryId: widget.categoryId,
+                      hasPassed: hasPassed,
+                    ),
+                  ),
+                  (Route<dynamic> route) => route.isFirst,
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void disableScreenshot() async {
@@ -79,6 +223,7 @@ class _TestscreenState extends State<Testscreen> {
   Widget build(BuildContext context) {
     final question = widget.questions[currentQuestionIndex];
     // Clone the list to avoid mutating the original data
+    bool isLastQuestion = currentQuestionIndex == widget.questions.length - 1;
 
 // Sort options by 'option_label' or any other key
     question.options.sort((a, b) => a.optionLabel.compareTo(b.optionLabel));
@@ -88,10 +233,17 @@ class _TestscreenState extends State<Testscreen> {
         title: Text(
             'Question ${currentQuestionIndex + 1} of ${widget.questions.length}'),
         centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: _showFinishConfirmationDialog,
+            child: const Text('Finish'),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
+          controller: _scrollController, // Add this line
           child: Column(
             children: [
               // Question Widget
@@ -115,13 +267,19 @@ class _TestscreenState extends State<Testscreen> {
                     apiService: _apiService,
                   )),
               // Explanation
-              if (showExplanation)
-                ExplanationWidget(
+
+              Visibility(
+                visible: showExplanation,
+                maintainSize: true,
+                maintainAnimation: true,
+                maintainState: true,
+                child: ExplanationWidget(
                   question: question,
                   licenceId: widget.licenceId,
                   categoryId: widget.categoryId,
                   apiService: _apiService,
                 ),
+              ),
             ],
           ),
         ),
@@ -148,7 +306,7 @@ class _TestscreenState extends State<Testscreen> {
               ),
               ElevatedButton(
                 onPressed: _nextQuestion,
-                child: const Text('Next'),
+                child: Text(isLastQuestion ? 'Finish' : 'Next'),
               ),
             ],
           ),
