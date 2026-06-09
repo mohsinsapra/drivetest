dc5a9fd9a9d1d1ae95186b292493079d81b58b9e
Muhammad Mohsin <mohsin.sapra@gmail.com>
2026-06-09 16:42:21 +0200

    chore: bump version to 1.5.16+295 [deploy]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2375466296" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "390503903" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
